19e5062964f3640968592d6c2c93f5504a2761d2
Lowella <lh850210@gmail.com>
2026-04-11 14:39:59 -0500

    Fetch ops data from API and temporary data display data for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import { Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [ops, setOps] = useState([]);
+
+  useEffect(() => {
+    const fetchOps = async () => {
+      const res = await fetch("https://frontend-challenge.veryableops.com/", {
+        headers: { Accept: "application/json" },
+        cache: "no-store",
+      });
+      const jsonData = await res.json();
+
+      setOps(jsonData);
+    };
+
+    fetchOps();
+  }, []);
+
   return (
     <Stack
       sx={{
@@ -10,7 +29,12 @@ export default function Home() {
         alignItems: "center",
       }}
     >
-      <Typography variant="h1">Hello World</Typography>
+      <Typography variant="h1">Ops</Typography>
+      {ops.map((op) => (
+        <div key={op.opId}>
+          <h3>{op.opTitle}</h3>
+        </div>
+      ))}
     </Stack>
   );
 }
